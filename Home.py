45ba03fe431dcde8fd5d9d8871f4386b9bdcd77e
Jimkelly Percine <jimkellypercine@brandeis.edu>
2025-01-25 18:35:02 -0500

    updated trend viz

--- Home.py
+++ Home.py
@@ -5,7 +5,10 @@ import plotly as pl
 import streamlit as st 
 import plotly.express as px
 import re
-
+from bs4 import BeautifulSoup
+import requests
+from ics import Calendar, Event
+from datetime import datetime
 
 #Function to add any chart to the page, and account for the click interactivity
 def addChartToPage(fig):
@@ -429,3 +432,15 @@ if uploaded_file is not None and st.session_state['checkFile'] == False:
         addChartToPage(pie_chart_non_members)
     
     st.subheader("Recent Event Trends", anchor="thirdSection")  
+    
+    
+    ### trends 
+    grouped_cost_revenue = df.groupby('eventName').agg({'Cost': 'sum', 'Revenue': 'sum'}).reset_index()
+
+    scatter_plot_cost = px.scatter(grouped_cost_revenue, 
+                          x='Cost', 
+                          y='Revenue', 
+                          color='eventName', 
+                          title='Cost vs. Revenue for Each Event')
+    
+    addChartToPage(scatter_plot_cost)
